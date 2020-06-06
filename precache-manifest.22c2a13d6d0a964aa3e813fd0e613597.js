54c70cfff1e78efcd2ed1578a006995596826da6
ATILAY <atilaymayadag@gmail.com>
2020-06-06 13:52:21 +0300

    Updates

---
+++ precache-manifest.22c2a13d6d0a964aa3e813fd0e613597.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809f7272f51efaa70e92e0fb98bc55c1",
+    "url": "/rnvsflutter/index.html"
+  },
+  {
+    "revision": "a77c78e1a46c591a5cb5",
+    "url": "/rnvsflutter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "33c5ec75c6e4d2b5a5c8",
+    "url": "/rnvsflutter/static/js/2.39edc8cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rnvsflutter/static/js/2.39edc8cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a77c78e1a46c591a5cb5",
+    "url": "/rnvsflutter/static/js/main.240037e1.chunk.js"
+  },
+  {
+    "revision": "953632bc52377eb5df74",
+    "url": "/rnvsflutter/static/js/runtime-main.5b6e7d18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rnvsflutter/static/media/logo.5d5d9eef.svg"
+  }
+]);
